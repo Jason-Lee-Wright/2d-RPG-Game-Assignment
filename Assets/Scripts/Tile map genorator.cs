b5fa0f455915315d7af848c2ad2fb62cf4d9fb80
Jason <jason974448@gmail.com>
2024-11-25 08:42:03 -0400

    Added the genorate map script from my previous project

--- Assets/Scripts/Tile map genorator.cs
+++ Assets/Scripts/Tile map genorator.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Tilemapgenorator : MonoBehaviour
 {
+    public Tilemap tilemap;
+    public Tile wallTile, doorTile, chestTile, floorTile, plantTile; // Tile assets for different map elements
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // Converts the generated map string into a Unity Tilemap
+    void ConvertMapToTilemap(string mapData)
     {
-        
+        tilemap.ClearAllTiles(); // Clear any existing tiles
+
+        string[] lines = mapData.TrimEnd().Split('\n');
+        int height = lines.Length;
+        int width = lines[0].Length;
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width - 1; x++) // Added - 1 as the tiles extended one past the limit giving us an index out of range error
+            {
+                Vector3Int tilePosition = new Vector3Int(x, height - y - 1, 0);
+                char tileChar = lines[y][x];
+
+                // Place tiles based on the character
+                switch (tileChar)
+                {
+                    case '#':
+                        tilemap.SetTile(tilePosition, wallTile);
+                        break;
+                    case 'O':
+                        tilemap.SetTile(tilePosition, doorTile);
+                        break;
+                    case '$':
+                        tilemap.SetTile(tilePosition, chestTile);
+                        break;
+                    case '.':
+                        tilemap.SetTile(tilePosition, floorTile);
+                        break;
+                    case '!':
+                        tilemap.SetTile(tilePosition, plantTile);
+                        break;
+                    default:
+                        tilemap.SetTile(tilePosition, floorTile);
+                        break;
+                }
+            }
+        }
+
+    }
+
+    void LoadPremadeLevel()
+    {
+
     }
 }
